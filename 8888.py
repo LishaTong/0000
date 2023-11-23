eafa7b258be12ebf2365cdcc53c3cd4ec697e86d
LishaTong <119466234+LishaTong@users.noreply.github.com>
2023-11-23 04:40:19 -0500

    Add files via upload

--- 8888.py
+++ 8888.py
@@ -6,6 +6,13 @@ def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 install('metaflow')
+import subprocess
+import sys
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+install('tensorflow')
 
 from metaflow import FlowSpec, step, IncludeFile, Parameter
 import pandas as pd
